a0334f964893056348c5b8ed7e810d80c70f748c
ihoryatseniuk <ihoryatseniuk@gmail.com>
2024-04-30 11:27:39 +0300

    Added show-hide functionality for modal window.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,26 @@
-// Functions======================================================
+// =========================================================================
+// =========================== Functions ===================================
+// =========================================================================
+
+const openModal = () => {
+  fromModalContainerElement.classList.remove("hidden");
+  overlayElement.classList.remove("hidden");
+  fromSearchInput.value = "";
+  ratesRenderer(rates);
+};
+
+const closeModal = () => {
+  fromModalContainerElement.classList.add("hidden");
+  fromSearchInput.value = "";
+  ratesRenderer(rates);
+};
 
 const attachListeners = (array) => {
-  // rates.forEach((element) => {
-  //   element.picked = false;
-  // });
   const liElements = document.querySelectorAll(".li-element");
   for (let i = 0; i < liElements.length; i++) {
     liElements[i].addEventListener("click", () => {
+      fromModalContainerElement.classList.add("hidden");
+      overlayElement.classList.add("hidden");
       array.forEach((element) => {
         element.picked = false;
       });
@@ -20,7 +34,6 @@ const attachListeners = (array) => {
 };
 
 const ratesRenderer = (array) => {
-  console.log(rates);
   listElement.innerHTML = "";
   for (let i = 0; i < array.length; i++) {
     const arrayObject = array[i];
@@ -30,6 +43,7 @@ const ratesRenderer = (array) => {
     <img src="${picked === true ? "img/picked.svg" : ""}" />
   </li>`;
   }
+  currencyButtonRenderer();
   attachListeners(array);
 };
 
@@ -40,22 +54,37 @@ const searchCurrency = (value) => {
   ratesRenderer(filteredArray);
 };
 
-// Functions======================================================
+const currencyButtonRenderer = () => {
+  rates.forEach((element) => {
+    if (element.picked) {
+      fromCurrencyElement.innerHTML = ` <img src="${element.img}"/><span>${element.name}</span
+      ><img src="img/arrow.svg" alt="arrow" />`;
+    }
+  });
+};
+
+// =========================================================================
+// =========================== Functions ===================================
+// =========================================================================
 
 const fromInputCurrencyContainer = document.querySelector(
   ".from-input-currency-container"
 );
 const fromInput = document.querySelector(".from-input");
 const fromSearchInput = document.querySelector(".search-container input");
+const fromCurrencyElement = document.querySelector(".from-currency");
+const fromModalContainerElement = document.querySelector(
+  ".from-modal-container"
+);
+const overlayElement = document.querySelector(".overlay");
+const listElement = document.querySelector(".list");
+
+ratesRenderer(rates);
 
 fromSearchInput.addEventListener("input", () => {
   const fromSearchInputValue = fromSearchInput.value.toUpperCase();
   searchCurrency(fromSearchInputValue);
 });
-
-const listElement = document.querySelector(".list");
-
-ratesRenderer(rates);
 
 fromInputCurrencyContainer.addEventListener("mouseover", () => {
   fromInputCurrencyContainer.classList.add("hover");
@@ -71,4 +100,15 @@ fromInput.addEventListener("focus", () => {
 
 fromInput.addEventListener("blur", () => {
   fromInputCurrencyContainer.classList.remove("focus");
+});
+
+fromCurrencyElement.addEventListener("click", () => {
+  fromModalContainerElement.classList.contains("hidden")
+    ? openModal()
+    : closeModal();
+});
+
+overlayElement.addEventListener("click", () => {
+  overlayElement.classList.add("hidden");
+  closeModal();
 });
--- scripts/rates.js
+++ scripts/rates.js
@@ -1,5 +1,5 @@
 const rates = [
-  { name: "CAD", img: "img/cad.svg", rate: 1.36, picked: true },
+  { name: "CAD", img: "img/cad.svg", rate: 1.36, picked: false },
   { name: "CNY", img: "img/cny.svg", rate: 7.24, picked: false },
   { name: "CZK", img: "img/czk.svg", rate: 23.53, picked: false },
   { name: "EUR", img: "img/eur.svg", rate: 0.93, picked: false },
@@ -8,5 +8,5 @@ const rates = [
   { name: "PLN", img: "img/pln.svg", rate: 4.024, picked: false },
   { name: "RUB", img: "img/rub.svg", rate: 93.22, picked: false },
   { name: "UAH", img: "img/uah.svg", rate: 40.0, picked: false },
-  { name: "USD", img: "img/usd.svg", rate: 1, picked: false },
+  { name: "USD", img: "img/usd.svg", rate: 1, picked: true },
 ];
